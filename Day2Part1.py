54de29693fec52ddd065ad0bcf0f86c06ec2b8b0
rkyatham01 <96209369+rkyatham01@users.noreply.github.com>
2021-12-17 02:38:07 -0500

    Add files via upload

---
+++ Day2Part1.py
@@ -0,0 +1,35 @@
+
+
+filebeingread = open("day2input.txt", "r")
+
+filehere = filebeingread.readlines()
+
+fileheregettingstripped = [line.rstrip() for line in filehere]
+
+countoftotal = 0
+
+for x in fileheregettingstripped:
+   countoftotal = countoftotal + 1
+   #countoftotal has the # of total lines read of file
+index = 0
+i = 0
+
+sumofdistance = 0
+sumofdepth = 0
+
+while i < countoftotal :
+    split1 = fileheregettingstripped[i]
+    splitline = split1.split(" ")
+    if splitline[0] == "forward":
+        sumofdistance = sumofdistance + int(splitline[1])
+    if splitline[0] == "down":
+        sumofdepth = sumofdepth + int(splitline[1])
+    if splitline[0] == "up":
+        sumofdepth = sumofdepth - int(splitline[1])
+    i = i + 1 #iterates through
+
+
+multiplyDepthAndSum = sumofdistance * sumofdepth
+print(multiplyDepthAndSum)
+
+
